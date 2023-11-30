b70f5df57c1a4b6b91bd7fcafc930cead7ca4dc4
“tsingbochun” <tsingbochun@gmail.com>
2023-11-30 15:19:06 +0800

    # The second, sum_cubes, computes the sum of the cubes of natural numbers up to n.
    # The second, sum_cubes, computes the sum of the cubes of natural numbers up to n.

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
-def sum_naturals(n):
-    total, k = 0, 1
-    while k <= n:
-        total, k = total + k, k + 1
-    return total
+# The second, sum_cubes, computes the sum of the cubes of natural numbers up to n.
+def sum_cubes(n):
+        total, k = 0, 1
+        while k <= n:
+            total, k = total + k*k*k, k + 1
+        return total
 
-sum_naturals(100)
-print(sum_naturals(100))
+print(sum_cubes(100))
